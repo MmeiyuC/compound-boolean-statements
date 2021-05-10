0d8ab4e3e03cb393b8e6965b4e5e4ebb8835a65c
MmeiyuC <>
2021-05-10 17:20:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let age
+let day
+
+// Connect "Submit" button to function
+document.getElementById('submit').addEventListener('click', ageandday)
+
+function ageandday () {
+  //Get user input and convert into an interger
+  age = document.getElementById('age').value
+  age = parseInt(age)
+
+  //Get user input day
+  day = document.getElementById('day').value
+
+  // Check if user can get student pricing
+  if ((day == 'Friday' || day == 'Saturday' || day == 'Sunday' || day == 'Monday') && (age > 12 && age < 21)) {
+    document.getElementById('answer').innerHTML = 'You can get the student pricing'
+  } else {
+    document.getElementById('answer').innerHTML = 'You must pay regular price'
+  }
+}
