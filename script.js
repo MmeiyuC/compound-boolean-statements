56f20c2c72e06aa2c62edb0076d4c8ba9086f0a1
MmeiyuC <>
2021-05-10 17:27:49 +0000

    edited

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let day
 document.getElementById('submit').addEventListener('click', ageandday)
 
 function ageandday () {
-  //Get user input and convert into an interger
+  // Get user input and convert into an interger
   age = document.getElementById('age').value
   age = parseInt(age)
 
-  //Get user input day
+  // Get user input day
   day = document.getElementById('day').value
 
   // Check if user can get student pricing
-  if ((day == 'Friday' || day == 'Saturday' || day == 'Sunday' || day == 'Monday') && (age > 12 && age < 21)) {
+  if ((day === 'Friday' || day === 'Saturday' || day === 'Sunday' || day === 'Monday') && (age > 12 && age < 21)) {
     document.getElementById('answer').innerHTML = 'You can get the student pricing'
   } else {
     document.getElementById('answer').innerHTML = 'You must pay regular price'
